88659eb22b1f971cab5da356b76f20a4cdbd36d5
ramonbezerra <ramon.bezerra@outlook.com>
2025-05-29 11:35:46 -0300

    fix: react root creation adjustment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React, { createRoot } from 'react-dom/client';
+import App from './components/App';
 
-ReactDOM.render(
-    <React.StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
         <App />
-    </React.StrictMode>,
-    document.getElementById("root")
 );
