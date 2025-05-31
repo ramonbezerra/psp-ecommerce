bd6ec06d90cc7860e3eeb47ec42130dc33e0ff79
ramonbezerra <ramon.bezerra@outlook.com>
2025-05-30 23:04:41 -0300

    feat: include router and provider into start app file

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthProvider from '../provider/authProvider';
+
+import ProtectedRoute from '../routes/ProtectedRoute';
+
 import Home from './Home';
+import Navbar from './Navbar';
+import Login from './Login';
+import Dashboard from './Dashboard';
 
 const App = () => {
     return (
         <div>
-            <h1>PSP E-commerce</h1>
-            <Home />
+            <h1 className='text-2xl'>PSP E-commerce</h1>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route element={<ProtectedRoute />}>
+                            <Route path="/dashboard" element={<Dashboard />} />
+                        </Route>
+                        <Route path="*" element={<div>NotFound</div>}/>
+                    </Routes>
+                </BrowserRouter>
+            </AuthProvider>
         </div>
     );
 }
