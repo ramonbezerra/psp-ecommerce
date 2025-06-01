6bcbea0d9255c751a4bcaceb70e298195b438d8d
ramonbezerra <ramon.bezerra@outlook.com>
2025-06-01 00:29:31 -0300

    feat: enhance login functionality with form validation using Formik and Yup

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,13 +1,32 @@
 import { useAuth } from '../provider/authProvider';
 import { useNavigate } from 'react-router-dom';
+import { Formik, ErrorMessage, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const LoginSchema = Yup.object().shape({
+    username: Yup.string()
+        .required('Username is required'),
+    password: Yup.string().min(4, 'Password must be at least 6 characters').max(120, 'Password must not exceed 120 characters')
+        .required('Password is required')
+});
 
 const Login = () => {
     const { setToken } = useAuth();
     const navigate = useNavigate();
 
     const handleLogin = () => {
-        setToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc0ODY2MTY5OSwianRpIjoiYzdlN2ZmYTktZTQwNS00NjBmLTkxOTAtMDU4ZmQ3MTgzMzZlIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImFsaWNlIiwibmJmIjoxNzQ4NjYxNjk5LCJjc3JmIjoiNjZiNDkzYzctOGI0OC00NWEyLWFmMWItMWFmNGJlODU2YzhkIiwiZXhwIjoxNzQ4NjYyNTk5LCJyb2xlIjoiYWRtaW4ifQ.VsTnHoegFl0w_d9EiZpYF_YdHVlbAS0kSD59kF4e9vE'); // Replace with actual token logic
-        navigate('/dashboard'); // Redirect to home after login
+        // setNestedObjectValues({ isValidating: true });
+        axios.post('http://localhost:5000/auth/login', { username: 'alice', password: 'admin' })
+            .then(response => {
+                setToken(response.data.access_token);
+                // setNestedObjectValues({ isValidating: false });
+                navigate('/dashboard');
+            })
+            .catch(error => {
+                // setNestedObjectValues({ isValidating: false });
+                console.error('Login failed:', error);
+            });
     }
 
     return (
